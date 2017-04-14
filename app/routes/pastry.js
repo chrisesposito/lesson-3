033d109812738c46fb06f8be87ffc7c1cb795484
Jeffrey Lembeck <jlembeck@gmail.com>
2017-04-14 06:38:40 -0700

    Starting point for application

---
+++ app/routes/pastry.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+
+const Pastry = require('../models/pastry')
+
+router.get('/', (req, res) => {
+})
+
+router.post('/', (req, res) => {
+})
+
+router.get('/:name', (req, res) => {
+})
+
+router.put('/:name', (req, res) => {
+})
+
+router.delete('/:name', (req, res) => {
+})
+
+module.exports = router
