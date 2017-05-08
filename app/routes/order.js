0c6599d45af172335fe459499f15bc16d6aa00aa
chris esposito <chris.esposito@gmail.com>
2017-05-07 21:10:09 -0700

    removed some questions from the comments, as I have answers

--- app/routes/order.js
+++ app/routes/order.js
@@ -8,9 +8,6 @@ const Order = require('../models/order')
 const methodOverride = require('method-override')
 router.use(methodOverride('_method'))
 
-// this is the current method for getting and then invoking the
-// rendering of the entire order - http://localhost:3000/orders
-// it currently complains about missing keys for list items
 router.get('/', (req, res) => {
     // Get the order here
     const getOrder = Order.getOrder();
@@ -23,9 +20,7 @@ router.get('/', (req, res) => {
     })
 })
 
-// this method appears to work, as I get a 201 status code
-// the idea was to then show the current order / basket,
-// but it blows up when it tries to render the page
+
 router.post('/pastries', (req, res) => {
     // Add an item to the order
     const {name} = req.body
